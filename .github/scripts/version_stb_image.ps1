de397b88a318053cd9f361958f42920b6dd56735
Lucas Girouard-Stranks <lustranks@gmail.com>
2021-07-26 18:10:24 -0400

    Update workflow

--- .github/scripts/version_stb_image.ps1
+++ .github/scripts/version_stb_image.ps1
@@ -1,5 +1,5 @@
 #!/usr/bin/env pwsh
 
-$regex='^.*(?<version>[0-9]+\.[0-9]+)\s+'
-$file='.\ext\stb\stb_image.h'
+$regex="^.*(?<version>[0-9]+\.[0-9]+)"
+$file=".\ext\stb\stb_image.h"
 select-string -Path $file -Pattern $regex -AllMatches | Select-Object -ExpandProperty Matches -First 1 | % { $_.Groups[1].Value }
