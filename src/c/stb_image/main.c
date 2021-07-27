8083d17c7f37b94d60ba429030591f849ac1092c
Lucas Girouard-Stranks <lustranks@gmail.com>
2021-07-26 23:54:55 -0400

    Fix export symbols for Windows

--- src/c/stb_image/main.c
+++ src/c/stb_image/main.c
@@ -3,6 +3,9 @@
         #error "Compiling for Windows 32-bit ARM or x86 is not supported."
     #endif
     #define VC_EXTRALEAN
+    #ifndef STB_IMAGE_STATIC
+        #define STBIDEF __declspec(dllexport)
+    #endif
 #endif
 
 #define STB_IMAGE_IMPLEMENTATION
